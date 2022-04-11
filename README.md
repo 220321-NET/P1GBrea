911a8a65d8daf6b0daf255663ca8c5b543b81e3d
Gianny <Gianny281@revature.net>
2022-04-11 19:19:27 -0400

    added .gitignore and program.cs

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# P1GBrea
-P1 Gianny
+Store App 2.0
+
+Into the Web
+
+Overview:
+The store app is a software that helps customers purchase products from your business. Designed with functionality that would make virtual shopping much simpler!
+
+Models:
+
+- Customer
+- StoreFront
+- Orders
+- Product
+- Inventory
+- LineItems - "Cart" (Individual product item with quantity in an order)
+
+Tech Stack:
+
+- C#
+- Xunit, Moq (for testing)
+- Serilog (for logging)
+- Azure (for our DB and for deployment)
+- Github Actions (pipeline)
+- ASP.NET (as our web framework)
+
+Bonus Features
+
+- Identity Handling (either manually or using ASP.NET Identity)
+- Web Frontend (either using CSS/HTML/JS or any other web frontend framework)
+- Containerize your backend
+- Email
+- and anything else you can think of!
---
+++ program.cs
@@ -0,0 +1,104 @@
+namespace PlantShop
+{
+    //main class
+    class StoreFront
+    {
+        //greeting method
+        GreetUser();
+    }
+    static void GreetUser()
+    {
+        //greet user and ask if they've shopped here before
+        Console.WriteLine("Welcome to PlantShop");
+        Console.WriteLine("Have you been here before?[Y/N]");
+        //get answer
+        string answer = Console.ReadLine().ToUpper();
+
+        if (answer == "Y")
+        {
+            SignIn();
+        }
+        else if (answer == "N")
+        {
+            SignUp();
+        }
+        else
+        {
+            return;
+        }
+
+    }
+    static void SignIn()
+    {
+        //ask for user name
+        Console.WriteLine("What is your username?");
+        string userName = Console.ReadLine();
+
+        //ask for password
+        Console.WriteLine("What is your password?");
+        string password = Console.ReadLine();
+
+        //verify userName and Password to sign in user
+        //if can't verify user ask to try again [y/n]
+        //if y take to signin
+        //if n take to signup
+
+        //greet returning user
+        Console.WriteLine("Welcome back ", userName);
+
+        //take to MainMenu
+    }
+
+    static void SignUp()
+    {
+        //ask user to create username
+        Console.WriteLine("What would you like your username to be?");
+        string userName = Console.ReadLine();
+
+        //make sure username is unique
+
+        //ask user to create password
+        Console.WriteLine("What would you like your password to be?");
+        string password = Console.ReadLine();
+
+        //create new user in db
+        //confirm user creation
+        Console.WriteLine("Your account has been created.");
+
+        //take to MainMenu
+    }
+    static void MainMenu()
+    {
+        Console.WriteLine("Welcome to Plant Shop!");
+        Console.WriteLine("[1] Buy Plants");
+        Console.WriteLine("[2] See Cart");
+        Console.WriteLine("[3] View Past Order");
+        Console.WriteLine("[x] Exit");
+
+        switch (cmResponse.Trim().ToUpper()[0])
+        {
+            case '1':
+                PlantMenu();
+                break;
+
+            case '2':
+                CartMenu();
+                break;
+
+            case '3':
+                OrderMenu();
+                break;
+
+            case 'X':
+                Exit = true;
+                break;
+
+            default:
+                Console.WriteLine("Invalid response, please try again.");
+                goto CustomerMenuInput;
+        }
+
+    } while (!customerExit);
+    }
+    }
+}
