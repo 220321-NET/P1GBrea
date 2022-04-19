4e9cddb974ebe0a42edac78f1ed6f5a32883a263
Gianny <Gianny281@revature.net>
2022-04-18 21:54:38 -0400

    fixed unhandled exception could not recognize connectionstring

--- PlantShop/UI/Program.cs
+++ PlantShop/UI/Program.cs
@@ -2,7 +2,7 @@
 using DL;
 using UI;
 
-string connectionString = File.ReadAllText("./connectString.txt");
+string connectionString = File.ReadAllText("./connectionstring.txt");
 IRepository repo = new DBRepo(connectionString);
 IPSBL bl = new PSBL(repo);
 new PlantShop(bl).Start();
